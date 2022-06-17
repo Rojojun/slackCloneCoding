6fd136e36b45b42bfac489fa9c9848d879b8edd8
Rojojun <72805486+Rojojun@users.noreply.github.com>
2022-06-17 21:38:23 +0900

    [feature] 첫 커밋

---
+++ src/main/java/com/clonecoding/slackclone/SlackCloneApplication.java
@@ -0,0 +1,13 @@
+package com.clonecoding.slackclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlackCloneApplication.class, args);
+    }
+
+}
